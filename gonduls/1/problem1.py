ae52194b4d7bdcb0592e319bd96d917b64369e3b
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-01 11:22:27 +0100

    day 1 end

--- gonduls/1/problem1.py
+++ gonduls/1/problem1.py
@@ -1,10 +1,8 @@
 import os
-i=0
 inputf = open('input.txt', 'r')
 inlist = inputf.readlines()
 for element in inlist:
-    inlist[i]=int(element[:-1])
-    i+=1
+    inlist[inlist.index(element)]=int(element[:-1])
 
 somma = 2020
 
