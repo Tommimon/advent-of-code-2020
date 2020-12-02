42452cf7e48ef407a5d4799de7514aa9b6c7736b
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2020-12-02 11:37:44 +0100

    day 2 part2

--- marcomole00/2/2.c
+++ marcomole00/2/2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #define DIM 1000
 int firstPart();
+int secondPart();
 
 
 int main()
 {
 
-printf("%d",firstPart());
+printf(" prima parte = %d\n",firstPart());
+printf("seconda parte = %d\n ", secondPart());
 
 }
 
@@ -34,5 +36,28 @@ int firstPart()
         if(numberOfOccurences >= n1 && numberOfOccurences <= n2) result++;
 
     }
+    fclose(fp);
     return result;
+}
+int secondPart()
+{
+    FILE *fp;
+    fp = fopen("input.txt","r");
+    int n1;
+    int n2;
+    char c;
+    char string[20];
+    int result = 0;
+
+    while (fscanf(fp, "%d-%d %c: %s",&n1,&n2,&c,string ) != EOF)
+    {
+        printf("%d-%d %c: %s \n", n1,n2,c,string);
+        if((string[n1-1]== c && string[n2-1] != c) ||  (string[n1-1]!= c && string[n2-1] == c)) result++; //it's just a dumbass xor
+
+    }
+    fclose(fp);
+
+    return result;
+
+
 }
