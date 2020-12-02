fe1ab8fbaefe3e702efee7d3a67aa48d290c6178
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-02 16:41:02 +0100

    mips pretty much sucks

--- Gonduls/input_Functions/conta_Byte.c
+++ Gonduls/input_Functions/conta_Byte.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define INPUT "/mnt/d/Pierluigi/programmazione/adventofcode/20/advent-of-code-2020/Gonduls/input_Functions/appunti.txt"
+#define INPUT "/mnt/d/Pierluigi/programmazione/adventofcode/20/advent-of-code-2020/Gonduls/2/input.txt"
 
 int main(){
     int c=1;
@@ -14,11 +14,11 @@ int main(){
     fclose(input);
     printf("Numero totale di byte: %d\n", result);
 
-    for (c=0; c<256; c++){
-        printf("Aschii #%d= %c", c, c);
-        if (c=="\n")
-            printf("\\n");
-        printf("\n");
-    }
+    //for (c=0; c<256; c++){
+    //    printf("Aschii #%d= %c", c, c);
+    //    if (c=="\n")
+    //        printf("\\n");
+    //    printf("\n");
+    //}
     return 0;
 }
