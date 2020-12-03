69489acf7ce231858da01452d4490666ddd92bbb
Riccardo Negri <67798955+riccardo-negri@users.noreply.github.com>
2020-12-03 10:18:03 +0100

    Update x.py

--- riccardo-negri/3/x.py
+++ riccardo-negri/3/x.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Day 3 solution of Advent Of Code 2020 by Riccardo Negri
-#First part answer: 153
-#Second part answer: 2421944712
+# First part answer: 153
+# Second part answer: 2421944712
 
 offsets = [[1,1], [3,1], [5,1], [7,1], [1,2]]	# x and y
 skip = False
@@ -13,7 +13,7 @@ with open('input.txt', 'r') as f:
 		x_coord = 0
 		skip = False
 		for line in f.readlines():
-			if (not skip):	# I use skip in case I need to increase y coordinates by more than one unit
+			if (not skip):		# I use skip in case I need to increase y coordinates by more than one unit
 				if offsets[case][1] == 2:
 					skip = True	
 				x_coord = x_coord % (len(line) - 1)
@@ -26,7 +26,7 @@ with open('input.txt', 'r') as f:
 		f.seek(0)
 
 for x in range(len(offsets)):
-	if x == 1:	# slope for part 1
+	if x == 1:				# slope for part 1
 		print("First part answer: " + str(offsets[x][2]))
 	answer *= offsets[x][2]
 print("Second part answer: " + str(answer))
