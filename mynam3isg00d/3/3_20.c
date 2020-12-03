88987a4a06806ab4ce7baad25e016bdd55f7ce61
mynam3isg00d <thedvd20@hotmail.com>
2020-12-03 16:59:30 +0100

    Optimized solution of d3

--- mynam3isg00d/3/3_20.c
+++ mynam3isg00d/3/3_20.c
@@ -1,42 +1,26 @@
-//I'm remaking it even shorter later, motherfuckers
-
+//Shortest c program I could get, WITHOUT CHEATING.
 #include<stdio.h>
-#define DIMX 31
-#define DIMY 323
 
-char map[DIMY][DIMX];
-int teamTrees(int xInc, int yInc);
-
-int main() {
-
-    FILE *fp;
-    fp = fopen("input.txt", "r");
-
-    int i = 0;
-    while(fscanf(fp, "%s", map[i])!=EOF) i++;   //fscanf sucks for this, it doesn't even break the lines, but it works so who cares
-
-    printf("1,1: %d\n", teamTrees(1, 1));
-    printf("3,1 (part 1 solution): %d\n", teamTrees(3, 1));
-    printf("5,1: %d\n", teamTrees(5, 1));
-    printf("7,1: %d\n", teamTrees(7, 1));
-    printf("1,2: %d\n", teamTrees(1, 2));
-
-    long int res = teamTrees(1, 1) * teamTrees(3, 1) * teamTrees(5, 1) * teamTrees(7, 1) * teamTrees(1, 2);
-    printf("--------\nFinal result: %ld", (long) res);  //this shit fucking works but how the FUCK do you print a long
-
-    system("PAUSE");
-    fclose(fp);
-    return 0;
-}
-
-int teamTrees(int xInc, int yInc) {
-    int x, y, gCounter;
-    x = y = gCounter = 0;
-    while(y<DIMY) {
-        if(map[y][x] == '#') gCounter++;
-        x += xInc;
-        if(x>=DIMX) x-=DIMX;
-        y += yInc;
+char input[10013];
+long long int trees(int x,int y) {
+    long long int p, rv, i; 
+    rv = p = i = 0;
+    while (p < 10013) {
+        if (input[p] == '#') rv++; 
+        i++;
+        p = 31*i*y + (i*(x+31*y))%31;
     }
-    return gCounter;
+    return rv;
+}
+int main() {
+    int i = 0;
+    char c;
+    FILE *fp = fopen("input.txt","r");
+    while((c = fgetc(fp)) != EOF) {
+        if(c != '\n') {
+            input[i] = c; 
+            i++;
+        }
+    }
+    printf("Part 1: %lld\nPart 2: %lld", trees(3,1), trees(1,1)*trees(3,1)*trees(5,1)*trees(7,1)*trees(1,2));
 }
