5bdf2181309419f9586f2277d29c50c6b2f41c8a
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-04 20:11:27 +0100

    day 4 done

--- Gonduls/4/problem2.py
+++ Gonduls/4/problem2.py
@@ -1,7 +1,7 @@
 import string
 with open('input.txt', 'r') as inputf:
     inlist = inputf.readlines()
-    inlist[-1]=inlist[-1]+' '   #serve uno spazio alla fine dell'ultimissima riga per far funzionare bene il codice, altrimenti si mangia un carattere
+    inlist[-1]=inlist[-1] + ' '   #serve uno spazio alla fine dell'ultimissima riga per far funzionare bene il codice, altrimenti si mangia un carattere
 
 def conditions(elem):
     if(not(len(elem.split(' '))==9 or (len(elem.split(' '))==8 and not ('cid' in elem)))):
@@ -9,26 +9,19 @@ def conditions(elem):
     elem = elem[:-1].split(' ')
     for info in elem:
         if (info[:3] == 'byr' and (len(info[4:])!=4 or (int(info[4:])<1920) or (int(info[4:])>2002))):
-            print(str(elem)+' 0')
             return 0
         if (info[:3] == 'iyr' and (len(info[4:])!=4 or (int(info[4:])<2010) or (int(info[4:])>2020))):
-            print(str(elem)+' 1')
             return 0
         if (info[:3] == 'eyr' and (len(info[4:])!=4 or (int(info[4:])<2020) or (int(info[4:])>2030))):
-            print(str(elem)+' 2')
             return 0
         if (info[:3] == 'pid' and (len(info[4:])!=9 or (not (info[4:].isdecimal())))):
-            print(str(elem)+' 3')
             return 0
         if (info[:3] == 'ecl' and (len(info[4:])!=3 or (not (info[4:] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])))):
-            print(str(elem)+' 4')
             return 0
         if (info[:3] == 'hcl' and (len(info[4:])!=7 or info[4] != '#' or not all(c in string.hexdigits for c in info[5:]))):
-            print(str(elem)+' 5')
             return 0
-        #if (info[:3] == 'hgt' and ()):
-        #    print(str(elem)+' 6')
-        #    return 0
+        if (info[:3] == 'hgt' and ((info[-2:]== 'cm' and (int(info[4:-2])<150 or int(info[4:-2])>193)) or (info[-2:]== 'in' and (int(info[4:-2])<59 or int(info[4:-2])>76)) or (info[-2:]!= 'cm' and info[-2:]!= 'in'))):
+            return 0
     return 1
 
 lista = ['']
