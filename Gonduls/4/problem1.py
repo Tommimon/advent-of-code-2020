8ed0b067bc9ad788361c1fa878c6f70939a3675f
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-04 11:02:23 +0100

    improved again

--- Gonduls/4/problem1.py
+++ Gonduls/4/problem1.py
@@ -9,9 +9,7 @@ for element in inlist:
     lista[-1]=lista[-1]+element[:-1]+' '
 
 risultato=0
-i=0
 for element in lista:
     if (len(element.split(' '))==9 or (len(element.split(' '))==8 and not ('cid' in element))):
         risultato += 1
-    i += 1
 print (risultato)
