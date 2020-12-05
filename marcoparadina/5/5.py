93fed54a7622b49959337529c35509b9befc7ba6
marcoparadina <mparadina10@gmail.com>
2020-12-05 22:33:39 +0100

    day 5 solution

---
+++ marcoparadina/5/5.py
@@ -0,0 +1,8 @@
+# The first line is a bit of an unneccessary flex but ok
+arr = [int( line.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0'), 2) for line in open('marcoparadina/5/input5.txt')]
+arr.sort()   
+for i in range(len(arr)):
+    if (arr[i+1])!=(arr[i]+1):
+        print('Solution part 2:', arr[i]+1)     # Since my ID is between two other missing IDs, in order to 
+           break                                # obtain mine i just add 1 to the one below mine, which is 
+                                                # the first missing one. 
