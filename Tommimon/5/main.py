039d9cef274518791f86d9407d0f330463b573ce
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-12-05 09:22:37 +0100

    Day 5 second part solution

--- Tommimon/5/main.py
+++ Tommimon/5/main.py
@@ -1,8 +1,12 @@
 with open('input.txt', 'r') as file:
-    lines = file.read().split('\n')
-    lines.remove('')
-    for i in range(len(lines)):
-        lines[i] = lines[i].replace('F', 'L').replace('L', '0')
-        lines[i] = lines[i].replace('B', 'R').replace('R', '1')
-        lines[i] = int(lines[i], 2)
-    print(max(lines))
+    seats = file.read().split('\n')
+    seats.remove('')
+    for i in range(len(seats)):
+        seats[i] = seats[i].replace('F', 'L').replace('L', '0')
+        seats[i] = seats[i].replace('B', 'R').replace('R', '1')
+        seats[i] = int(seats[i], 2)
+    print(max(seats))
+    seats.sort()
+    for i in range(len(seats)-1):
+        if(seats[i+1]-seats[i] == 2):
+            print(seats[i]+1)
