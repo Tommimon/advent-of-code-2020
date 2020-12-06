07a660b3df4f5d02431e9fb9309b2c99cd8d6342
marcoparadina <mparadina10@gmail.com>
2020-12-06 19:09:54 +0100

    day 6 both parts

---
+++ marcoparadina/6/6.py
@@ -0,0 +1,22 @@
+import string
+anyone_answered=0
+everyone_answered=0
+group=[]
+check=[1]*(ord('z')-ord('a')+1)
+with open('marcoparadina/6/input6.txt') as f:
+    for line in f:
+        if line == '\n':
+            everyone_answered+=sum(check)
+            check=[1]*(ord('z')-ord('a')+1)
+            for letter in string.ascii_lowercase:
+                if letter in group:
+                    anyone_answered+=1
+            group=[]
+        else:
+            group+=line
+            for letter in (string.ascii_lowercase):
+                    if letter not in line:
+                        #print('duce')
+                        check[ord('a')-ord(letter)]=0
+print('Solution to part :', anyone_answered)
+print('Solution to part 2:', everyone_answered)
