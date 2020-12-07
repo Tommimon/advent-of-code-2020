5278bc8144c826a6ef7ebce00e50fcd12f3491d4
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-07 19:04:44 +0100

    day 7 complete

--- Gonduls/7/problem2.py
+++ Gonduls/7/problem2.py
@@ -3,17 +3,17 @@ with open('input.txt') as f:
     for line in f:
         lista.append([line.split(' contain')[0][:-1],line.split(' contain')[1][1:].replace('.','').replace('\n','').replace('bags','bag').split(', ')])
 
-counted = []
 def recursive_counting(stringa):
     partial_sol=1
     if(stringa == 'shiny gold bag'):
         partial_sol=0   #shiny gold bag doesn't count as a solution
     for line in lista:
-        for el in line[1]:
-            if (stringa in el[2:] and (line[0] not in counted)):
-                counted.append(line[0])
-                partial_sol= recursive_counting(line[0]) + partial_sol
-
+        if (line[0] == stringa):
+            if(line[1][0] == 'no other bag'):
+                return 1
+            for el in line[1]:
+                partial_sol = int(el[0]) * recursive_counting(el[2:]) + partial_sol
+            break
     return partial_sol
 
 print (recursive_counting('shiny gold bag'))
