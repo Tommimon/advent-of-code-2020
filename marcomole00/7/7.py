44ffc8c353ae17808604e6189c85f6b92d10ce67
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2020-12-07 23:57:12 +0100

    day 7 part2, good night

--- marcomole00/7/7.py
+++ marcomole00/7/7.py
@@ -1,10 +1,30 @@
+myBag = 'shiny gold'
+rules = []
+midBags = set()
+midBags.add(myBag)
+#i've the feeling that never attended an algo class is making this too much fucking difficult
+#pradella i'm ready
 
-myBag = 'shiny gold '
-bags = set()
-bags.add(myBag)
-bagsTemp = bags.union() #during the iteration you can't change 
-with open('marcomole00/7/input.txt') as file:
-    rules = file.read().split('\n')
+def part2(string):
+    numberTemp = 1
+    if string == myBag: numberTemp = 0
+    for line in rules:
+        outerBag, innerBags = line.split(' bags contain ')
+        innerBags = innerBags.replace(', ', '.').replace('bags', '').replace('bag', '').split('.')[:-1]
+        if outerBag == string:
+            for idk in innerBags:
+                if idk != 'no other ':
+                    #print(idk)
+                    idk = idk[:-1]
+                    print(idk)
+                    numberTemp += int(idk[0])*int(part2(idk[2:]))
+    return(numberTemp)
+            
+
+def part1(rules):
+    bags = set()
+    bags.add(myBag)
+    bagsTemp = bags.union() #during the iteration you can't change 
     while True:
         check = False
         for bag in bags:
@@ -14,9 +34,15 @@ with open('marcomole00/7/input.txt') as file:
                     bagsTemp.add(outerBag)
         if len(bags) == len(bagsTemp):break
         bags = bags.union(bagsTemp)
+    bags.remove(myBag)
+    print(len(bags))
+
+with open('marcomole00/7/input.txt') as file:
+    rules = file.read().split('\n')
+    part1(rules)
+    #print(midBags)
+    print(part2(myBag))
         
-bags.remove(myBag)
-print(len(bags))
         
         
         
