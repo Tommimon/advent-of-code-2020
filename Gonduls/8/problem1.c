24dbe277697ebc6964e2c8c482e089a2a0286c54
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-08 16:52:23 +0100

    input day 8

--- Gonduls/8/problem1.c
+++ Gonduls/8/problem1.c
@@ -7,21 +7,36 @@
 typedef struct{
     char op[4];
     int value;
+    int seen;
 }operation;
 
 void input(operation* array, int limit);
-void stampa (operation array[], int limit);
+//void stampa (operation array[], int limit);
 
 int main (){
     operation array[MAX];
     input(array, MAX);
-    stampa(array, MAX);
+    //stampa(array, MAX);
     return 0;
 }
 
-void stampa (operation array[], int limit){
+/*void stampa (operation array[], int limit){
     int i;
     for (i=0; i<limit; i++){
-        printf("")
+        printf("%s %d\n", array[i].op, array[i].value);
     }
+}
+*/
+
+void input(operation* array, int limit){
+    int i, error = 0;
+    FILE * input;
+    input = fopen(INPUT, "r");
+    while (i<limit && error != EOF){
+        fscanf(input, "%s %d", array[i].op, &array[i].value);
+        error = fgetc(input);
+        i++;
+    }
+    if (i != limit && error != EOF)
+        printf("an error may have occured\n");
 }
