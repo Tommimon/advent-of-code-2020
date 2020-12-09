506320d0c70ba07c1a822ee06f9e3bdd893c91ed
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2020-12-09 18:14:56 +0100

    removed bloat from 9
    helo

--- marcomole00/9/9.py
+++ marcomole00/9/9.py
@@ -1,32 +1,24 @@
 with open('marcomole00/9/input.txt')as file:
-    lines = file.read().split('\n')
-    lines = list(map(int,lines))
+    lines = list(map(int,file.read().split('\n')))
     for i in range(len(lines)-25):
         check = False
         k = i+25
         for j in range(25):
-            #print(lines[k-j-1])
             for l in range(25):
                 if (k == l): break
                 if lines[k] == lines[k-j-1] +lines[k-l-1]:check = True
-                #print(lines[k-j-1] +lines[k-l-1])
         if check == False:
-            print('ijjjj', lines[k])
+            print('first:', lines[k])
             endpoint = lines[k]
-            linesBefore = lines[:k+1]
             break
-    print(linesBefore[0], ' ', linesBefore[-1] )
-    arrayNumbers = []
     n = 0
-    for number in linesBefore:
-        del arrayNumbers[:]
+    for number in lines:
+        arrayNumbers = []
         end = False
         i= 0
-        for i in range(len(linesBefore) -n):
-            #print(len(linesBefore) -n)
-            arrayNumbers.append(linesBefore[i+n])
+        for i in range(len(lines) -n):
+            arrayNumbers.append(lines[i+n])
             if sum(arrayNumbers) == endpoint:
-                print('helo')
                 end = True
                 break
             elif sum(arrayNumbers) > endpoint: 
