3f24ab5804a62485f89c09a61587817c262ff797
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-12-09 10:37:28 +0100

    Remove useless breaks

--- Tommimon/9/day9.py
+++ Tommimon/9/day9.py
@@ -1,5 +1,4 @@
 offset = 25
-invalid = 0
 with open('input.txt', 'r') as file:
     numbers = list(map(int, file.read().split('\n')))
 for i, num in enumerate(numbers[offset:]):
@@ -11,17 +10,14 @@ for i, num in enumerate(numbers[offset:]):
                     found = True
     if not found:
         invalid = num
-        break
 print(invalid)
 summation = 0
 start = 0
 end = 0
-while True:
+while summation != invalid:
     if summation < invalid:
         summation += numbers[end]
         end += 1
-    if summation == invalid:
-        break
     elif summation > invalid:
         summation -= numbers[start]
         start += 1
