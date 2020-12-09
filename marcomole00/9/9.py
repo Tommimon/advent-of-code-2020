f594f39de6de8c3598638fc87eecbe885d59c1d4
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2020-12-09 10:45:59 +0100

    day9 both parts

---
+++ marcomole00/9/9.py
@@ -0,0 +1,48 @@
+lines = []
+
+
+
+with open('marcomole00/9/input.txt')as file:
+    lines = file.read().split('\n')
+    lines = list(map(int,lines))
+    for i in range(len(lines)-25):
+        check = False
+        k = i+25
+        for j in range(25):
+            #print(lines[k-j-1])
+            for l in range(25):
+                if (k == l): break
+                if lines[k] == lines[k-j-1] +lines[k-l-1]:check = True
+                #print(lines[k-j-1] +lines[k-l-1])
+        if check == False:
+            print('ijjjj', lines[k])
+            endpoint = lines[k]
+            linesBefore = lines[:k+1]
+            break
+    print(linesBefore[0], ' ', linesBefore[-1] )
+    arrayNumbers = []
+    n = 0
+    for number in linesBefore:
+        temp = 0
+        del arrayNumbers[:]
+        end = False
+        i= 0
+        for i in range(len(linesBefore) -n):
+            #print(len(linesBefore) -n)
+            arrayNumbers.append(linesBefore[i+n])
+            if sum(arrayNumbers) == endpoint:
+                print('helo')
+                end = True
+                break
+            elif sum(arrayNumbers) > endpoint: 
+                break
+        if end == True: 
+            print(min(arrayNumbers) + max(arrayNumbers))
+            break
+        n = n+1
+
+
+            
+
+        
+                        
