f1b9a8b92fb2e096cc4f2922dd2cc4cf5ca4d10c
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-10 10:55:41 +0100

    it's a start

---
+++ Gonduls/10/problem2.py
@@ -0,0 +1,13 @@
+with open('input.txt', 'r') as inputf:
+        lista= list(map(int, inputf.read().split('\n')))
+lista.append(0)
+lista=sorted(lista)
+lista.append(lista[-1]+3)
+diff_one = 0
+diff_three = 0
+for i, num in enumerate(lista[:-1]):
+    if (lista[i+1] == num + 3):
+        diff_three +=1
+    elif (lista[i+1] == num + 1):
+        diff_one +=1
+print(diff_one*diff_three)
