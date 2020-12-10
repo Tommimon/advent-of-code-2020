a53ede1b847082bdd9411458a071e93e721ed1e2
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-10 19:23:25 +0100

    day 10 not working

--- Gonduls/10/probem2point0.py
+++ Gonduls/10/probem2point0.py
@@ -7,8 +7,5 @@ def conta(parlist, offset = 0):
         #print(parlist, offset)
         partial = 1
         for i in range(len(parlist[1+offset:-1])):
-                if (parlist[offset+i+2]-parlist[i+offset]<=3):
-                        partial += conta(parlist[:i+1+offset]+parlist[i+2+offset:], i+offset)
-        return partial
-
+                
 print(conta(lista))
