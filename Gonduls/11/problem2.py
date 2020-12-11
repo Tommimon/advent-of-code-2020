95ba5ed1bb62c0463a9a484c96da29c8a9e5b06b
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-11 18:49:11 +0100

    day 11 complete

--- Gonduls/11/problem2.py
+++ Gonduls/11/problem2.py
@@ -9,18 +9,26 @@ def change(matrix):
             if(el == '.'):
                 result[-1] = result[-1] + '.'
                 continue
-            adjacent = [[j-1,i-1],[j-1,i],[j-1,i+1],[j+1,i-1],[j+1,i],[j+1,i+1],[j,i-1],[j,i+1]]
-            letters = []
-            for direction in adjacent:
-                if (len(line)>direction[1] >=0 and len(matrix)>direction[0]>=0):
-                    letters.append(matrix[direction[0]][direction[1]])
+            letters = seen (matrix, [j,i])
             if(letters.count('#')==0):
                 result[-1] = result[-1] + '#'
-            elif(letters.count('#')>3):
+            elif(letters.count('#')>4):
                 result[-1] = result[-1] + 'L'
             else:
                 result[-1] = result[-1] + el
     return result[:]
+
+def seen(matrix, coordinates):
+    directions = [(1,1),(1,0),(1,-1),(0,1),(0,-1),(-1,1),(-1,0),(-1,-1)]
+    ris =''
+    for elem in directions:
+        directions = [coordinates[0]+elem[0],coordinates[1]+elem[1]]
+        while (len(matrix[0])>directions[1] >=0 and len(matrix)>directions[0]>=0):
+            if (matrix[directions[0]][directions[1]] != '.'):
+                ris = ris + matrix[directions[0]][directions[1]]
+                break
+            directions = [directions[0]+elem[0],directions[1]+elem[1]]
+    return ris
 
 while(True):
     newlista = change(lista[:])
