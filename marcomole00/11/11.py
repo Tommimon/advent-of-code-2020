4c25830678c3b7e5208da90017590b00257d44ff
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2020-12-11 15:10:30 +0100

    day 11 - part1

--- marcomole00/11/11.py
+++ marcomole00/11/11.py
@@ -1,85 +1,95 @@
+# i beg you to shoot in my face and public shame my dead body
 
-
-with open('marcomole00/11/test') as file:
+import copy
+with open('marcomole00/11/input.txt') as file:
     lines = file.read().split('\n')
     lines = list(map(list, lines))
-    cop = []
-    old = 0
+    print(len(lines))
 
-    
+old = 0   
 while True:
-    #print('ciao')
-    copia = list(map(list, map(tuple, lines)))
+   # cop = copy.deepcopy(lines)
+    cop2 = copy.deepcopy(lines)
+   # copia = list(map(list, map(tuple, lines)))
     for i in  range(len(lines)):
-    
         for j in range(len(lines[i])):
-            seatsOccupied = [0]*8 
-            if lines[i][j] == '.': break
+            seatsFree = 0
+            max = 0
+            seatsOccupied = [0]*8
             if lines[i][j] == 'L':
-                try:
-                    if lines[i-1][j] != '#': seatsOccupied[0] = 0
-                except IndexError: pass
-                try:
-                    if lines[i-1][j+1] != '#': seatsOccupied[1] = 0
-                except IndexError: pass
-
-                try:
-                    if lines[i-1][j-1] != '#': seatsOccupied[2] = 0
-                except IndexError: pass
-                try:
-                    if lines[i][j+1] != '#': seatsOccupied[3] = 0
-                except IndexError: pass
-                try:
-                    if lines[i][j-1]!= '#': seatsOccupied[4] = 0
-                except IndexError: pass
-                try:
-                    if lines[i+1][j]!= '#': seatsOccupied[5] = 0
-                except IndexError: pass
-                try:
-                    if lines[i+1][j-1] != '#': seatsOccupied[6] = 0
-                except IndexError: pass
-                try:
-                    if lines[i+1][j+1] != '#': seatsOccupied[7] = 0
-                except IndexError: pass
-                if (sum(seatsOccupied)) == 0: 
-                    lines[i][j] = '#'
+                if i>0:
+                    max +=1
+                    if lines[i-1][j] != '#': 
+                        seatsFree += 1
+                if i>0 and  j< len(lines[i])-1:
+                    max +=1
+                    if lines[i-1][j+1] != '#': 
+                        seatsFree +=1
+                if i>0 and  j>0 :
+                    max +=1
+                    if lines[i-1][j-1] != '#': 
+                        seatsFree += 1
+                if j< len(lines[i])-1:
+                    max +=1
+                    if lines[i][j+1] != '#': 
+                        seatsFree += 1
+                if j>0:
+                    max +=1
+                    if lines[i][j-1]!= '#': 
+                        seatsFree += 1
+                if i < len(lines) -1 :
+                    max +=1
+                    if lines[i+1][j]!= '#': 
+                        seatsFree += 1
+                if i < len(lines) -1 and j>0 :
+                    max +=1
+                    if lines[i+1][j-1] != '#': 
+                        seatsFree += 1
+                if i < len(lines) -1 and j< len(lines[i]) -1:
+                    max +=1
+                    if lines[i+1][j+1] != '#': 
+                        seatsFree += 1
+                if seatsFree == max: 
+                    cop2[i][j] = '#'
             
             elif lines[i][j] == '#':
-                try:
-                    if lines[i-1][j] == '#': seatsOccupied[0] = 1
-                except IndexError: pass
-                try:
-                    if lines[i-1][j+1] == '#': seatsOccupied[1] = 1
-                except IndexError: pass
-
-                try:
-                    if lines[i-1][j-1] == '#': seatsOccupied[2] = 1
-                except IndexError: pass
-                try:
-                    if lines[i][j+1] == '#': seatsOccupied[3] = 1
-                except IndexError: pass
-                try:
-                    if lines[i][j-1] == '#': seatsOccupied[4] = 1
-                except IndexError: pass
-                try:
-                    if lines[i+1][j] == '#': seatsOccupied[5] = 1
-                except IndexError: pass
-                try:
-                    if lines[i+1][j-1] == '#': seatsOccupied[6] = 1
-                except IndexError: pass
-                try:
-                    if lines[i+1][j+1] == '#': seatsOccupied[7] = 1
-                except IndexError: pass
+            
+                if i>0 and lines[i-1][j] == '#':
+                     seatsOccupied[0] = 1
+                if i>0 and  j< len(lines[i])-1 and lines[i-1][j+1] == '#': 
+                    seatsOccupied[1] = 1
+                if i>0 and  j>0 and lines[i-1][j-1] == '#':
+                     seatsOccupied[2] = 1
+                if j< len(lines[i]) -1 and  lines[i][j+1] == '#': 
+                    seatsOccupied[3] = 1
+                if j>0 and  lines[i][j-1] == '#': 
+                    seatsOccupied[4] = 1
+                if i < len(lines)-1 and lines[i+1][j] == '#': 
+                    seatsOccupied[5] = 1
+                if i < len(lines)-1 and j>0 and lines[i+1][j-1] == '#':
+                     seatsOccupied[6] = 1
+                if i < len(lines) -1 and j< len(lines[i]) -1 and  lines[i+1][j+1] == '#': 
+                    seatsOccupied[7] = 1
+    
                 if (sum(seatsOccupied)) >= 4: 
-                    lines[i][j] = 'L'
+                    cop2[i][j] = 'L'
     add = 0
+    lines = copy.deepcopy(cop2)
+    cop2 = []
     for line in lines: 
-        for let in line: 
-            if let == '#': add+=1
-    if old == add: break #why this doesn't work padre pio porco maledetto
-    else: old = add 
+        for lett in line: 
+            if lett == '#': add += 1
+    print(add)
+    if old == add: 
+        break #why this doesn't work padre pio porco maledetto
+    else: old = add   
 
 
+for row in lines: print(row)
+add = 0
+for line in lines: 
+    for let in line: 
+        if let == '#': add+=1
 print(add)
 
 
