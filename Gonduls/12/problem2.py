5ae08b38e61dcd9717a90a8107b7153b3511ab8a
Gonduls <pierluigi.negro.00@gmail.com>
2020-12-12 12:31:49 +0100

    day 12 done

--- Gonduls/12/problem2.py
+++ Gonduls/12/problem2.py
@@ -2,15 +2,24 @@ with open('input.txt', 'r') as inputf:
         lista= list(inputf.read().split('\n'))
 cardinals = ['N','E','S','W','N','E','S','W']
 directions = {'N':[1,0],'E':[0,1],'S':[-1,0],'W':[0,-1]}
-position = [[0,0],'E']
+position = [0,0]
+waypoint = [[1,'N'],[10,'E']]
 for line in lista:
     if (line[0] in directions.keys()):
-        position = [[position[0][0] + directions[line[0]][0]*int(line[1:]),position[0][1] + directions[line[0]][1]*int(line[1:])], position[1]]
+        waypoint = [[waypoint[0][0] + directions[line[0]][0]*int(line[1:]), 'N'] , [waypoint[1][0] + directions[line[0]][1]*int(line[1:]), 'E']]
+        if(waypoint[0][0]<0):
+            waypoint[0][1]='S'
+        if(waypoint[1][0]<0):
+            waypoint[1][1]='W'
     if (line[0] == 'R'):
-        position[1] = cardinals[cardinals.index(position[1]) + int(int(line[1:])/90)]
+        waypoint = [[waypoint[0][0], cardinals[cardinals.index(waypoint[0][1]) + int(int(line[1:])/90)]] , [waypoint[1][0], cardinals[cardinals.index(waypoint[1][1]) + int(int(line[1:])/90)]]]
     if (line[0] == 'L'):
-        position[1] = cardinals[cardinals.index(position[1]) - int(int(line[1:])/90)]
+        waypoint = [[waypoint[0][0], cardinals[cardinals.index(waypoint[0][1]) - int(int(line[1:])/90)]] , [waypoint[1][0], cardinals[cardinals.index(waypoint[1][1]) - int(int(line[1:])/90)]]]
+    if(waypoint[0][1] == 'E' or waypoint[0][1] == 'W'):
+        waypoint = [waypoint[1],waypoint[0]]
+    for ind, way in enumerate(waypoint):
+        waypoint[ind][0]= abs(waypoint[ind][0]) * directions[way[1]][ind]
     if (line[0] == 'F'):
-        position = [[position[0][0] + directions[position[1]][0]*int(line[1:]),position[0][1] + directions[position[1]][1]*int(line[1:])], position[1]]
-    #print(position[0][0], ' ', position[0][1], ' ', position[1])
-print(abs(position[0][0])+abs(position[0][1]))
+        position = [position[0] + waypoint[0][0]*int(line[1:]) , position[1] + waypoint[1][0]*int(line[1:])]
+
+print(abs(position[0])+abs(position[1]))
