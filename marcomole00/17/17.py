0eb7511843b3529d81b0d58b4e11527fd9cddff4
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2020-12-17 21:22:34 +0100

    day 17 p2

--- marcomole00/17/17.py
+++ marcomole00/17/17.py
@@ -2,54 +2,37 @@ from copy import deepcopy
 with open('marcomole00/17/input.txt') as file:
     lines = file.read().split('\n')
 space = {}
-""" space = {    #(x,y,z)
-    (0,1,0) : '#',
-    (1,0,0) : '#',
-    (-1,-1,0) : '#',
-    (0,-1,0) : '#',
-    (1,-1,0) : '#'
-} """
-print(lines)
-
+#for part1 just delete all mentions of w and dw
 for i in range(len(lines)):
     for j in range(len(lines[0])):
-        space[(i,j,0)] = lines[i][j]
-#spaceCopy = deepcopy(space)
-""" for key in spaceCopy:
-    (x,y,z) = key
-    for dx in (-1,0,1):
-        for dy in (-1,0,1):
-            for dz in (-1,0,1):
-                if (x+dx,y+dy,z+dz) not in spaceCopy: 
-                    space[(x+dx,y+dy,z+dz)] = '.' """
-
-
+        space[(i,j,0,0)] = lines[i][j]
 
 for i in range(6):
     spaceCopy = deepcopy(space)
     for key in spaceCopy:
-        (x,y,z) = key
+        (x,y,z,w) = key
         for dx in (-1,0,1):
             for dy in (-1,0,1):
                 for dz in (-1,0,1):
-                    if (x+dx,y+dy,z+dz) not in spaceCopy: 
-                        space[(x+dx,y+dy,z+dz)] = '.'
+                    for dw in (-1,0,1):
+                        if (x+dx,y+dy,z+dz,w+dw) not in spaceCopy: 
+                            space[(x+dx,y+dy,z+dz,w+dw)] = '.'
     
     spaceCopy = deepcopy(space)
     
     #populate the neighbors
     for key in spaceCopy:
-        (x,y,z) = key
+        (x,y,z,w) = key
         
         active = 0
-        inactive = 0
         for dx in (-1,0,1):
             for dy in (-1,0,1):
                 for dz in (-1,0,1):
-                    if dx == dy == dz == 0: continue
-                    try:
-                        if spaceCopy[(x+dx,y+dy,z+dz)] == '#': active +=1
-                    except KeyError: pass
+                    for dw in (-1,0,1):
+                        if dx == dy == dz == dw == 0: continue
+                        try:
+                            if spaceCopy[(x+dx,y+dy,z+dz,w+dw)] == '#': active +=1
+                        except KeyError: pass
 
         if spaceCopy[key] == '#': 
             if active != 2 and active !=3:
