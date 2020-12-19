4c62664a563109783e04806f052b6e7d58133c7e
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-12-19 18:11:35 +0100

    Remove useless code

--- Tommimon/19/day19.py
+++ Tommimon/19/day19.py
@@ -1,36 +1,24 @@
 def match(string, rule):
     global rules_list
-    if len(string) == 0:
+    if len(rule) == 0:  # if no rule always match
+        return {0}  # with this I sum 0 and I keep the previous lengths
+    elif len(string) == 0:  # never match
         return set()
-    elif len(rule) == 1:
-        if isinstance(rule[0], str):
-            if string[0] == rule[0]:
-                return {1}
-            else:
-                return set()
-        else:
-            return match(string, rules_list[rule[0]])
+    elif isinstance(rule[0], str):
+        if string[0] == rule[0]:
+            return {1}
     elif -1 in rule:
         separator = rule.index(-1)
         return match(string, rule[:separator]).union(match(string, rule[separator+1:]))
-    elif len(rule) == 2:
-        l1 = match(string, rules_list[rule[0]])
+    else:
         possible = set()
+        l1 = match(string, rules_list[rule[0]])
         for i in l1:
-            l2 = match(string[i:], rules_list[rule[1]])
+            l2 = match(string[i:], rule[1:])
             for j in l2:
                 possible.add(i + j)
         return possible
-    elif len(rule) == 3:
-        l1 = match(string, rules_list[rule[0]])
-        possible = set()
-        for i in l1:
-            l2 = match(string[i:], rules_list[rule[1]])
-            for j in l2:
-                l3 = match(string[i+j:], rules_list[rule[2]])
-                for k in l3:
-                    possible.add(i + j + k)
-        return possible
+    return set()  # if rule is string but different
 
 
 with open('input.txt', 'r') as file:
@@ -39,11 +27,11 @@ rules_list = []
 for r in paragraph[0].split('\n'):
     parts = r.replace(':', '').replace('|', '-1').replace('"', '').split(' ')
     index = int(parts[0])
-    rules_list += [None] * (index - len(rules_list) + 1)
+    rules_list += [None] * (index - len(rules_list) + 1)  # resize list to reach new index
     try:
         rules_list[index] = list(map(int, parts[1:]))
     except ValueError:
-        rules_list[index] = [parts[1]]
+        rules_list[index] = [parts[1]]  # if is "a" or "b"
 
 counter = 0
 for m in paragraph[1].split('\n'):
