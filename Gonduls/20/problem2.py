aa6d9950a9903783996d2c29a55976efd841871e
Gonduls <pierluigi.negro.00@gmail.com>
2021-02-17 22:44:20 +0100

    built

--- Gonduls/20/problem2.py
+++ Gonduls/20/problem2.py
@@ -1,4 +1,5 @@
 lenght_line = 10
+lenght_matrix = 12
 class Tile:
     def __init__(self, number, matrix):
         self.id = int(number)
@@ -23,14 +24,12 @@ class Tile:
         self.left_mir = conversion_to_num(left[::-1])
         self.right_mir = conversion_to_num(right[::-1])
         self.sides = [self.up, self.right, self.down, self.left, self.up_mir, self.right_mir, self.down_mir, self.left_mir]
-        
-        # I have to keep track wether a tile has been mirrored yet or not
-        self.mirrored = False
+
     
 
     # only prints sides and id
     def print_tile(self):
-        print('Tile ', self.id)
+        #print('Tile ', self.id)
         print(conversion_to_string(self.up))
         to_print_left = (conversion_to_string(self.left_mir))
         to_print_right = (conversion_to_string(self.right))
@@ -54,13 +53,30 @@ class Tile:
             self.matrix = [[self.matrix[line][column] for line in range(lenght_line-1,-1,-1)] for column in range(lenght_line)]
             self.matrix = ["".join(self.matrix[line]) for line in range(lenght_line)]
             self.up, self.right, self.down, self.left =  self.left, self.up, self.right, self.down
-            self.up_mir, self.left_mir, self.down_mir, self.right_mir =  self.right_mir, self.up_mir, self.left_mir, self.down_mir
+            #self.up_mir, self.left_mir, self.down_mir, self.right_mir =  self.right_mir, self.up_mir, self.left_mir, self.down_mir
+            left = ''.join([self.matrix[-line][0] for line in range(1, lenght_line+1)])
+            right = ''.join([self.matrix[line][-1] for line in range(lenght_line)])
+            self.up_mir = conversion_to_num(self.matrix[0][::-1])
+            self.down_mir = conversion_to_num(self.matrix[-1])
+            self.left_mir = conversion_to_num(left[::-1])
+            self.right_mir = conversion_to_num(right[::-1])
 
     
     # I only need to mirror it around the x axis (I just switch the lines so that the first line is the last and viceversa)
     def mirror(self):
         self.matrix = [self.matrix[line] for line in range(lenght_line-1, -1, -1)]
-        self.mirrored = True
+        self.up = conversion_to_num(self.matrix[0])
+        self.down = conversion_to_num(self.matrix[-1][::-1])
+        left = ''.join([self.matrix[-line][0] for line in range(1, lenght_line+1)])
+        right = ''.join([self.matrix[line][-1] for line in range(lenght_line)])
+        self.left = conversion_to_num(left)
+        self.right = conversion_to_num(right)
+        self.up_mir = conversion_to_num(self.matrix[0][::-1])
+        self.down_mir = conversion_to_num(self.matrix[-1])
+        self.left_mir = conversion_to_num(left[::-1])
+        self.right_mir = conversion_to_num(right[::-1])
+        self.sides = [self.up, self.right, self.down, self.left, self.up_mir, self.right_mir, self.down_mir, self.left_mir]
+
 
 #conversion from binary (#,.) to int of sides, occupies less space
 def conversion_to_num(string):
@@ -110,7 +126,6 @@ for el in tile_list:
     if unmatched == 2:
         answer *= el.id
         corners.append([tile_list.index(el), corner_sides])
-
 #print(answer)
 ############### start part 2 ####################
 
@@ -131,25 +146,32 @@ if len(tiles_matrix) != 1:
     tile_list[corners[0]].rotate(3 - corners[0][1][0])
     tiles_matrix.append([corners[0][0]])
 
-#print(corners)
-# I have to build the actual matrix
-for line in range(lenght_line):
-    for column in range(1, lenght_line):
-        # previous_tile is the tile's index in tile_list
+used = [corners[0][0]]
+# I have to build the matrix for the tiles indices
+for line in range(lenght_matrix):
+    for column in range(1, lenght_matrix):
+        # previous_tile is the previous tile's index in tile_list
         previous_tile = tiles_matrix[line][column - 1]
         right = tile_list[previous_tile].right
         # tile is an actual tile, not an index
         for index, tile in enumerate(tile_list):
-            if(index != previous_tile) and right in tile.sides:
+            
+            if index != previous_tile and right in tile.sides:
                 if right in tile.sides[:4]:
                     tile.mirror()
+                    
                 while(right != tile.left_mir):
                     tile.rotate(1)
                 tiles_matrix[line].append(index)
-    if line != lenght_line - 1:
-        down = tile_list[matrix[-1][0]].down
+                tile_list[index].print_tile
+                used.append(index)
+                break
+    
+    # If I'm at the last line there won't be another tile that matches the down sides anymore
+    if line != lenght_matrix - 1:
+        down = tile_list[tiles_matrix[-1][0]].down
         for index, tile in enumerate(tile_list):
-            if(index != matrix[-1][0]) and down in tile.sides:
+            if(index != tiles_matrix[-1][0]) and down in tile.sides:
                 if down in tile.sides[4:]:
                     tile.mirror()
                 while(down != tile.down):
@@ -157,6 +179,18 @@ for line in range(lenght_line):
                 tile.mirror()
                 tiles_matrix.append([])
                 tiles_matrix[-1].append(index)
+                used.append(index)
                 break
-        
-        
+
+# Now I have to build the actual image
+image = []
+for line in range(lenght_matrix):
+    # I'm building the image line by line, each line in tiles_matrix has 8 small lines
+    for small_line in range (lenght_line - 2):
+        image.append([''])
+        for column in range(lenght_matrix):
+            part = tile_list[tiles_matrix[line][column]].matrix[small_line + 1][1:-1]
+            #print(part, image[-1])
+            image[-1][0] = image[-1][0] + part
+        print(image[-1][0])
+
