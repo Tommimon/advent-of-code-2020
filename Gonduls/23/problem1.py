c2e0be9558f8e7c9e0515c827cb6fc5a9842986b
Gonduls <pierluigi.negro.00@gmail.com>
2021-02-21 17:14:25 +0100

    Made part 1 a bit better

--- Gonduls/23/problem1.py
+++ Gonduls/23/problem1.py
@@ -3,16 +3,15 @@ cups = list(map(lambda el: int(el), str(input))) #showoff
 curr = cups[0]
 for i in range (100):
     # three consecutive elements from curr
-    a, b, c = cups[cups.index(curr)-8], cups[cups.index(curr)-7], cups[cups.index(curr)-6]
-    cups.remove(a)
-    cups.remove(b)
-    cups.remove(c)
+    three = [cups[cups.index(curr)-8], cups[cups.index(curr)-7], cups[cups.index(curr)-6]]
+    for el in three:
+        cups.remove(el)
     dest = curr -1
-    while(dest not in cups and dest>0):
+    while(dest in three and dest>0):
         dest -= 1
-    if dest not in cups:
+    if dest == 0:
         dest = max(cups)
-    cups = cups[:cups.index(dest)+1] + [a,b,c] + cups[cups.index(dest)+1:]
+    cups = cups[:cups.index(dest)+1] + three + cups[cups.index(dest)+1:]
     curr = cups[cups.index(curr)-8]
     
 print(str(cups[cups.index(1)+1:]+cups[:cups.index(1)])[1:-1].replace(', ',''))
