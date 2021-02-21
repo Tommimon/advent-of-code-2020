043d4bfb9a4a4dcd5eb4dc7f6ef955e9ba3bd000
Gonduls <pierluigi.negro.00@gmail.com>
2021-02-21 11:44:54 +0100

    how not to solve a problem under 11-12 hours (day 23)

--- Gonduls/23/problem2.py
+++ Gonduls/23/problem2.py
@@ -1,20 +1,24 @@
+#how not to solve a problem under 11-12 hours
 input = 389125467
+max_l = 1000000
 cups = list(map(lambda el: int(el), str(input))) #showoff
 curr = cups[0]
 cups.extend(list(range(10, 1000001)))
 for i in range (10000000):
-    print(i)
-    # three consecutive elements from curr
-    a, b, c = cups[cups.index(curr)-1000000 +1], cups[cups.index(curr)-1000000 +2], cups[cups.index(curr)-1000000 +3]
-    cups.remove(a)
-    cups.remove(b)
-    cups.remove(c)
+    ind = cups.index(curr)
+    letters = []
+    for y in range(3):
+        letters.append(cups[ind-(max_l-y) +1])
+        del cups[ind -(max_l-y) +1]
     dest = curr -1
-    while(dest not in cups and dest>0):
+    while(dest in letters and dest>0):
         dest -= 1
-    if dest not in cups:
-        dest = max(cups)
-    cups = cups[:cups.index(dest)+1] + [a,b,c] + cups[cups.index(dest)+1:]
-    curr = cups[cups.index(curr)-1000000+1]
+    if dest == 0:
+        dest = max_l
+        while dest in letters:
+            dest -= 1
+    ind_dest = cups.index(dest)
+    cups = cups[:ind_dest+1] + letters + cups[ind_dest +1:]
+    curr = cups[cups.index(curr)-max_l+1]
     
-print(cups[cups.index(1) -1000000 +1] * cups[cups.index(1) -1000000 +2])
+print(cups[cups.index(1) -max_l +1] * cups[cups.index(1) -max_l +2])
