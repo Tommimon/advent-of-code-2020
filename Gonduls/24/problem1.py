5914b4711a653d2858fd11ecbf5d5f19e2098fb2
Gonduls <pierluigi.negro.00@gmail.com>
2021-02-22 18:25:06 +0100

    day 24 complete, now let'd do better then 2 min 15 s

---
+++ Gonduls/24/problem1.py
@@ -0,0 +1,40 @@
+with open('./Gonduls/24/input.txt') as inputf:
+    lines = inputf.read().split('\n')
+
+paths = []
+for line in lines:
+    paths.append(line.replace('e', 'e ').replace('w', 'w ').split(' '))
+    if '' in paths[-1]:
+        paths[-1].remove('')
+
+coordinates = []
+for line in paths:
+    print(line)
+    print('e:',line.count('e'), ' w:',line.count('w'), ' ne:',line.count('ne'), ' se:',line.count('se'), ' nw:',line.count('nw'), ' sw:',line.count('sw'))
+    coords = [0,0]
+    for el in line:
+        if el == 'e':
+            coords[0] += 2
+        if el == 'w':
+            coords[0] -= 2
+        if el == 'ne':
+            coords[0] += 1
+            coords[1] += 1
+        if el == 'se':
+            coords[0] += 1
+            coords[1] -= 1
+        if el == 'nw':
+            coords[0] -= 1
+            coords[1] += 1
+        if el == 'sw':
+            coords[0] -= 1
+            coords[1] -= 1
+    coordinates.append(coords)
+answer = []
+for line in coordinates:
+    if line in answer:
+        answer.remove(line)
+    else:
+        answer.append(line)
+
+print(len(answer))
---
+++ Gonduls/24/problem2.py
@@ -0,0 +1,57 @@
+with open('./Gonduls/24/input.txt') as inputf:
+    lines = inputf.read().split('\n')
+
+paths = []
+for line in lines:
+    paths.append(line.replace('e', 'e ').replace('w', 'w ').split(' '))
+    if '' in paths[-1]:
+        paths[-1].remove('')
+
+coordinates = []
+for line in paths:
+    coords = [0,0]
+    for el in line:
+        if el == 'e':
+            coords[0] += 2
+        if el == 'w':
+            coords[0] -= 2
+        if el == 'ne':
+            coords[0] += 1
+            coords[1] += 1
+        if el == 'se':
+            coords[0] += 1
+            coords[1] -= 1
+        if el == 'nw':
+            coords[0] -= 1
+            coords[1] += 1
+        if el == 'sw':
+            coords[0] -= 1
+            coords[1] -= 1
+    coordinates.append(tuple(coords))
+tiles = []
+for line in coordinates:
+    if line in tiles:
+        tiles.remove(line)
+    else:
+        tiles.append(line)
+
+#print(len(tiles))
+#print((tiles))
+
+for i in range(100):
+    max_x = max(list(map(lambda el: abs(el[0]), tiles)))
+    max_y = max(list(map(lambda el: abs(el[1]), tiles)))
+    new_tiles = []
+    for x in range(-max_x -2, max_x + 2):
+        for y in range(-max_y -2, max_y + 2):
+            if((x+y)%2):
+                continue
+            adjacent = [(x+2,y), (x-2,y), (x+1,y+1), (x+1,y-1), (x-1,y+1), (x-1,y-1)]
+            if (x,y) in tiles:
+                if (list(map(lambda el: el in tiles, adjacent)).count(True) in (1,2)):
+                    new_tiles.append((x,y))
+            else:
+                if(list(map(lambda el: el in tiles, adjacent)).count(True) == 2):
+                    new_tiles.append((x,y))
+    tiles = new_tiles
+print(len(tiles))
